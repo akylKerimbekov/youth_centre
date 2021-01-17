5a73b0495b392300cf06a86cf7b9a17446a23370
Akyl Kerimbekov <akyl.kerimbekov@gmail.com>
2021-01-17 17:07:12 +0300

    add fxml file and controller

---
+++ src/main/java/dev/akyl/youthcentre/presentation/controller/AddRequestController.java
@@ -0,0 +1,13 @@
+package dev.akyl.youthcentre.presentation.controller;
+
+import javafx.event.ActionEvent;
+
+public class AddRequestController {
+    public void btnAddRequest(ActionEvent actionEvent) {
+
+    }
+
+    public void btnCancelRequest(ActionEvent actionEvent) {
+
+    }
+}
