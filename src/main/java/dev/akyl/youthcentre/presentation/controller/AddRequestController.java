7c5c068d535231c0df2b374c025c6939de931d6d
Akyl Kerimbekov <akyl.kerimbekov@gmail.com>
2021-01-29 12:56:38 +0300

    add request add and update

--- src/main/java/dev/akyl/youthcentre/presentation/controller/AddRequestController.java
+++ src/main/java/dev/akyl/youthcentre/presentation/controller/AddRequestController.java
@@ -1,20 +1,34 @@
 package dev.akyl.youthcentre.presentation.controller;
 
-import dev.akyl.youthcentre.repository.entity.Request;
-import dev.akyl.youthcentre.repository.entity.Teenager;
+import dev.akyl.youthcentre.repository.entity.*;
+import dev.akyl.youthcentre.service.DeliveryServiceRefService;
+import dev.akyl.youthcentre.service.HardLifeService;
+import dev.akyl.youthcentre.service.PsychoActiveReferenceService;
 import dev.akyl.youthcentre.service.RequestService;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
-public class AddRequestController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddRequestController implements Initializable {
     private ObservableList<Request> appRequestObservableList;
     private Teenager teenager;
 
+    @FXML
+    public ComboBox<DeliveryServiceRef> cbDeliveryService;
+    @FXML
+    public ComboBox<PsychoActiveRef> cbPsychoActive;
+    @FXML
+    public ComboBox<HardLifeRef> cbHardLife;
     @FXML
     TextField tfNum;
     @FXML
@@ -26,6 +40,7 @@ public class AddRequestController {
     @FXML
     TextArea taSupport;
 
+
     public Teenager getTeenager() {
         return teenager;
     }
@@ -36,6 +51,70 @@ public class AddRequestController {
 
     public ObservableList<Request> getAppRequestObservableList() {
         return appRequestObservableList;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //cbPsychoActive.getItems().addAll(PsychoActiveReferenceService.getInstance().findAll());
+        //cbHardLife.getItems().addAll(HardLifeService.getInstance().findAll());
+        //cbDeliveryService.getItems().addAll(DeliveryServiceRefService.getInstance().findAll());
+        cbPsychoActive.setItems(PsychoActiveReferenceService.getInstance().findAll());
+        cbPsychoActive.setConverter(new StringConverter<PsychoActiveRef>() {
+            @Override
+            public String toString(PsychoActiveRef psychoActiveRef) {
+                if (psychoActiveRef != null) {
+                    return psychoActiveRef.getCode();
+                }
+                return "";
+            }
+
+            @Override
+            public PsychoActiveRef fromString(String s) {
+                return cbPsychoActive.getItems()
+                        .stream()
+                        .filter(ap -> ap.getCode().equals(s))
+                        .findFirst()
+                        .orElse(null);
+            }
+        });
+        cbDeliveryService.setItems(DeliveryServiceRefService.getInstance().findAll());
+        cbDeliveryService.setConverter(new StringConverter<DeliveryServiceRef>() {
+            @Override
+            public String toString(DeliveryServiceRef deliveryServiceRef) {
+                if (deliveryServiceRef != null) {
+                    return deliveryServiceRef.getCode();
+                }
+                return "";
+            }
+
+            @Override
+            public DeliveryServiceRef fromString(String s) {
+                return cbDeliveryService.getItems()
+                        .stream()
+                        .filter(ap -> ap.getCode().equals(s))
+                        .findFirst()
+                        .orElse(null);
+            }
+        });
+        cbHardLife.setItems(HardLifeService.getInstance().findAll());
+        cbHardLife.setConverter(new StringConverter<HardLifeRef>() {
+            @Override
+            public String toString(HardLifeRef hardLifeRef) {
+                if (hardLifeRef != null) {
+                    return hardLifeRef.getCode();
+                }
+                return "";
+            }
+
+            @Override
+            public HardLifeRef fromString(String s) {
+                return cbHardLife.getItems()
+                        .stream()
+                        .filter(ap -> ap.getCode().equals(s))
+                        .findFirst()
+                        .orElse(null);
+            }
+        });
     }
 
     public void setAppRequestObservableList(ObservableList<Request> appRequestObservableList) {
@@ -50,6 +129,9 @@ public class AddRequestController {
         request.setConsultation(taConsultation.getText());
         request.setDeviation(taDeviation.getText());
         request.setSupport(taSupport.getText());
+        request.setDeliveryServiceRef(cbDeliveryService.getSelectionModel().getSelectedItem());
+        request.setHardLifeRef(cbHardLife.getSelectionModel().getSelectedItem());
+        request.setPsychoActiveRef(cbPsychoActive.getSelectionModel().getSelectedItem());
         RequestService.getInstance().save(request);
         closeStage(actionEvent);
     }
@@ -59,8 +141,8 @@ public class AddRequestController {
     }
 
     private void closeStage(ActionEvent event) {
-        Node source = (Node)  event.getSource();
-        Stage stage  = (Stage) source.getScene().getWindow();
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
         stage.close();
     }
 }
--- src/main/java/dev/akyl/youthcentre/presentation/controller/MainController.java
+++ src/main/java/dev/akyl/youthcentre/presentation/controller/MainController.java
@@ -1,28 +1,31 @@
 package dev.akyl.youthcentre.presentation.controller;
 
-import dev.akyl.youthcentre.repository.entity.Request;
-import dev.akyl.youthcentre.repository.entity.Teenager;
+import dev.akyl.youthcentre.repository.entity.*;
 import dev.akyl.youthcentre.service.RequestService;
 import dev.akyl.youthcentre.service.TeenagerService;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.ObjectBinding;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
@@ -43,9 +46,22 @@ public class MainController implements Initializable {
     @FXML
     TableView<Request> request;
     @FXML
-    TableColumn<Request, Long> requestId;
-    @FXML
     TableColumn<Request, String> requestNum;
+    @FXML
+    public TableColumn<Request, HardLifeRef> requestHardLife;
+    @FXML
+    public TableColumn<Request, PsychoActiveRef> requestPsychoActive;
+    @FXML
+    public TableColumn<Request, DeliveryServiceRef> requestDeliveryService;
+    @FXML
+    public TableColumn<Request, String> requestAddiction;
+    @FXML
+    public TableColumn<Request, String> requestDeviation;
+    @FXML
+    public TableColumn<Request, String> requestConsult;
+    @FXML
+    public TableColumn<Request, String> requestSupport;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -62,8 +78,60 @@ public class MainController implements Initializable {
             }
         });
 
-        requestId.setCellValueFactory(new PropertyValueFactory<Request, Long>("id"));
         requestNum.setCellValueFactory(new PropertyValueFactory<Request, String>("number"));
+        requestAddiction.setCellValueFactory(new PropertyValueFactory<Request, String>("addiction"));
+        requestConsult.setCellValueFactory(new PropertyValueFactory<Request, String>("consult"));
+        requestDeviation.setCellValueFactory(new PropertyValueFactory<Request, String>("deviation"));
+        requestSupport.setCellValueFactory(new PropertyValueFactory<Request, String>("support"));
+        requestHardLife.setCellValueFactory(new PropertyValueFactory<Request, HardLifeRef>("hardLifeRef"));
+        requestHardLife.setCellFactory(new Callback<TableColumn<Request, HardLifeRef>, TableCell<Request, HardLifeRef>>(){
+            @Override
+            public TableCell<Request, HardLifeRef> call(TableColumn<Request, HardLifeRef> param) {
+                TableCell<Request, HardLifeRef> cell = new TableCell<Request, HardLifeRef>(){
+                    @Override
+                    protected void updateItem(HardLifeRef item, boolean empty) {
+                        if (item != null) {
+                            Label cityLabel = new Label(item.getCode());
+                            setGraphic(cityLabel);
+                        }
+                    }
+                };
+                return cell;
+            }
+        });
+        requestPsychoActive.setCellValueFactory(new PropertyValueFactory<Request, PsychoActiveRef>("psychoActiveRef"));
+        requestPsychoActive.setCellFactory(new Callback<TableColumn<Request, PsychoActiveRef>, TableCell<Request, PsychoActiveRef>>() {
+            @Override
+            public TableCell<Request, PsychoActiveRef> call(TableColumn<Request, PsychoActiveRef> requestPsychoActiveRefTableColumn) {
+                TableCell<Request, PsychoActiveRef> cell = new TableCell<Request, PsychoActiveRef>() {
+                    @Override
+                    protected void updateItem(PsychoActiveRef psychoActiveRef, boolean b) {
+                        if (psychoActiveRef != null) {
+                            Label label = new Label(psychoActiveRef.getCode());
+                            setGraphic(label);
+                        }
+                    }
+                };
+                return cell;
+            }
+        });
+        requestDeliveryService.setCellValueFactory(new PropertyValueFactory<Request, DeliveryServiceRef>("deliveryServiceRef"));
+        requestDeliveryService.setCellFactory(new Callback<TableColumn<Request, DeliveryServiceRef>, TableCell<Request, DeliveryServiceRef>>() {
+            @Override
+            public TableCell<Request, DeliveryServiceRef> call(TableColumn<Request, DeliveryServiceRef> requestDeliveryServiceRefTableColumn) {
+                TableCell<Request, DeliveryServiceRef> cell = new TableCell<Request, DeliveryServiceRef>(){
+                    @Override
+                    protected void updateItem(DeliveryServiceRef deliveryServiceRef, boolean b) {
+                        if (deliveryServiceRef != null) {
+                            Label label = new Label(deliveryServiceRef.getCode());
+                            setGraphic(label);
+                        }
+                    }
+                };
+                return cell;
+            }
+        });
+
         request.setItems(RequestService.getInstance().findAll());
         request.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
             if (newSelection != null) {
@@ -111,7 +179,7 @@ public class MainController implements Initializable {
         Teenager temp = teenager.getSelectionModel().getSelectedItem();
         dialogController.setTeenager(temp);
 
-        Scene scene = new Scene(parent, 500, 300);
+        Scene scene = new Scene(parent, 500, 500);
         Stage stage = new Stage();
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setScene(scene);
